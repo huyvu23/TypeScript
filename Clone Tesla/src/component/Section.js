7afd76bc362727662e49f37632a57736980845a2
huyvu23 <vuh32616@gmail.com>
2022-04-27 23:24:39 +0700

    add

---
+++ Clone Tesla/src/component/Section.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+      <Button>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {/* Nếu có right button thì sẽ hiển thị */}
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Button>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  // background-image: url("/images/model-3.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+// Cho phép nút bên phải kế thừa nút bên trái
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: #333;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Button = styled.div``;
