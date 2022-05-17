baef63743f26479ac8655cbf17ae9d08801485b4
huyvu23 <vuh32616@gmail.com>
2022-05-17 23:03:03 +0700

    add

---
+++ ReactJS_2/Demo Redux/demo_redux_reactjs/src/Action/test.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const test = createSlice({
+  name: second,
+  initialState,
+  reducers: {},
+});
+
+export const {} = test.actions;
+
+export default test.reducer;
